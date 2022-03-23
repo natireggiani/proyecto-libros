3d92365ca946f34360b7df1b93b4df430b6a3883
Natalia Reggiani <nreggiani77@gmail.com>
2022-03-23 14:49:17 -0300

    inclui NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import NavBar from './Components/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 export default function App() {
 
   return (
     <>
+      
       <NavBar/>
 
   
