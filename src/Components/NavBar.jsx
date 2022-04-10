6178291e48746559a65f571fab5690f55c8b7c64
Natalia Reggiani <nreggiani77@gmail.com>
2022-04-10 20:21:08 -0300

    pre entrega

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Navbar, Container, Nav}from 'react-bootstrap';
 import logo from '../Images/logo.jpg';
 import CartWidget from './CartWidget';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
 
@@ -10,19 +11,19 @@ export default function NavBar() {
     <header className='barra'>
         <Navbar bg="white" expand="lg">
           <Container>
-            <Navbar.Brand href="/">
+            <Navbar.Brand as={Link} to="/">
               <img src={logo}
                   width="30"
                   height="30"
                   alt='Logo'
-                  className="d-inline-block align-top"/>{' '}     
+                  className="d-inline-block align-top"/>{' '}  
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" className="me-auto" />
             <Navbar.Collapse id="basic-navbar-nav" >
               <Nav className="me-auto">
-                <Nav.Link href ="/categoria/Astronave" className='barra-links'>Colección Astronave</Nav.Link>
-                <Nav.Link href="/categoria/RyG" className='barra-links'>Roque y Gervasio</Nav.Link>
-                <Nav.Link href="/categoria/FC" className='barra-links'>Fuera de Colección</Nav.Link>  
+                <Nav.Link  as={Link} to="/categoria/Astronave" className='barra-links'>Colección Astronave</Nav.Link>
+                <Nav.Link as={Link} to="/categoria/RyG" className='barra-links'>Roque y Gervasio</Nav.Link>
+                <Nav.Link as={Link} to="/categoria/FC" className='barra-links'>Fuera de Colección</Nav.Link>  
               </Nav>
             </Navbar.Collapse>
             <CartWidget />
